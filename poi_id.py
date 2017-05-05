352f352f0e81e18c33447f5b334c6b8f3319d26a
Ramon Prieto <ramonprieto12@gmail.com>
2017-05-04 22:04:18 -0500

    final naive bayes model

--- poi_id.py
+++ poi_id.py
@@ -2,54 +2,104 @@
 
 import sys
 import pickle
+import matplotlib.pyplot
 sys.path.append("/Users/ramonprieto/Google Drive/Data Analysis/machine-learning/mini-projects/ud120-projects/tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
+from sklearn.cross_validation import train_test_split
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] 
+features_list = ['poi'] 
+excluded_features = ["loan_advances", "email_address", "poi"]
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
 ### Task 2: Remove outliers
+#Remove the total from the data 
+data_dict.pop("TOTAL", 0)
+data_dict.pop("THE TRAVEL AGENCY IN THE PARK", 0)
+
 ### Task 3: Create new feature(s)
+for name in data_dict:
+	from_this_person_to_poi = data_dict[name]["from_this_person_to_poi"]
+	from_messages = data_dict[name]["from_messages"]
+	from_poi_to_this_person = data_dict[name]["from_poi_to_this_person"]
+	to_messages = data_dict[name]["to_messages"]
+	
+	if from_messages=="NaN" or to_messages=="NaN":
+		data_dict[name]["from_poi_ratio"] = 0
+		data_dict[name]["to_poi_ratio"] = 0
+	else:
+		data_dict[name]["from_poi_ratio"] = float(from_this_person_to_poi)/(from_messages+from_this_person_to_poi)
+		data_dict[name]["to_poi_ratio"] = float(from_poi_to_this_person)/(to_messages+from_poi_to_this_person)
+
+#Get all features into feature_list
+for name in data_dict:
+	for feature in data_dict[name]:
+		if feature in excluded_features:
+			continue
+		else:
+			features_list.append(feature)
+	break
+
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
 
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
+
+
 labels, features = targetFeatureSplit(data)
 
-### Task 4: Try a varity of classifiers
-### Please name your classifier clf for easy export below.
-### Note that if you want to do PCA or other multi-stage operations,
-### you'll need to use Pipelines. For more info:
-### http://scikit-learn.org/stable/modules/pipeline.html
-
-# Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
-
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
-### using our testing script. Check the tester.py script in the final project
-### folder for details on the evaluation method, especially the test_classifier
-### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
-### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
-
-# Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
+#Split train and test data
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
 
-### Task 6: Dump your classifier, dataset, and features_list so anyone can
+#Select and tune algorithm
+from sklearn.feature_selection import SelectKBest
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV, StratifiedShuffleSplit
+from sklearn.metrics import accuracy_score, recall_score, precision_score
+from sklearn.naive_bayes import GaussianNB
+cv = StratifiedShuffleSplit(n_splits = 1000, test_size = .3, random_state = 42)
+
+scaler = MinMaxScaler()
+selector = SelectKBest(k = 4)
+pca = PCA(n_components = 15)
+gnb = GaussianNB()
+pipeline = Pipeline([("selector", selector), ("pca", pca), ("gnb", gnb)])
+
+#parameters to be changed in grid search
+params_grid =  {
+				"pca__n_components": [4], 
+				"selector__k": [15], 
+				}
+
+gs = GridSearchCV(pipeline, params_grid, cv = cv, scoring = "f1")
+gs.fit(features, labels)
+
+
+#Select optimal classifier
+print gs.best_estimator_
+clf = gs.best_estimator_
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+
+### Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
 ### generates the necessary .pkl files for validating your results.
-
 dump_classifier_and_data(clf, my_dataset, features_list)
+
+#Test your results using test classifier
+from tester import test_classifier
+test_classifier(clf, my_dataset, features_list)
